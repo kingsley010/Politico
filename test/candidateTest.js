e03da6df86ae5cd287fb1fe66c44e945c9d4c819
Kingsley <perfectman090@gmail.com>
2019-02-07 20:26:17 +0100

    b

--- test/candidateTest.js
+++ test/candidateTest.js
@@ -31,30 +31,6 @@ describe('POST Requests', () => {
         });
       });
 
-      // describe ('POST /api/v1/auth/signup', () => {
-      //   it('should create a new user', (done) => {
-      //     request(app)
-      //       .post('/api/v1/auth/signup')
-      //       .send({
-      //         firstname: 'Jessica',
-      //         lastname: 'Chizoba',
-      //         othername: 'Zizi',
-      //         email: 'Jessy@gmail.com',
-      //         phonenumber: '+234-12345678',
-      //         passporturl: 'meg.jpg',
-      //         password: 'meggy',
-      //       })
-      //       .end((err, res) => {
-      //         expect(res.statusCode).to.equal(201);
-      //         expect(res.body).to.be.an('object');
-      //         expect(res.body.data).to.be.an('array');
-      //         expect(res.body.data[0]).to.be.an('object');
-      //         token = res.body.data[0].token;
-      //       done();
-      //       });
-      //   });
-      // });
-
       describe ('POST /api/v1/offices/id/register', () => {
         it('should create a new candidate', (done) => {
           request(app)
@@ -66,7 +42,6 @@ describe('POST Requests', () => {
               userid: 2
             })
             .end((err, res) => {
-              console.log(res.body);
               expect(res.statusCode).to.equal(201);
               expect(res.body).to.be.an('object');
               expect(res.body.data).to.be.an('array');
