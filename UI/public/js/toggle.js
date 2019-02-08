2a2450c8a1086686a8187d758866bb81d1f75f0b
Kingsley <perfectman090@gmail.com>
2019-02-08 07:30:15 +0100

    gh-pages

--- UI/public/js/toggle.js
+++ UI/public/js/toggle.js
@@ -7,21 +7,4 @@ function toggleMenu() {
     } else header.className = 'header-links';
 }
 
-/*Code to toggle 'add comment' box*/
-
-let addComment = document.getElementsByClassName("add-comment-button");
-let addCommentCancel = document.getElementsByClassName("comment-cancel-button");
-let commentSections = document.getElementsByClassName("comment-section");
-
-for (let i = 0; i <= addComment.length - 1; i++) {
-    addComment.item(i).addEventListener('click', toggleCommentSection);
-    addCommentCancel.item(i).addEventListener('click', toggleCommentSection);
-    
-    function toggleCommentSection() {
-        let comment = commentSections.item(i);
-
-        if (comment.style.display === 'block') comment.style.display = 'none';
-            else comment.style.display = 'block';
-    }
-}
 /*---------------------------------------------------*/
