e95a544ed58b25c5088e5e8367e3c5804adc2fb4
Kingsley <perfectman090@gmail.com>
2019-02-10 11:16:59 +0100

    chore(server): set up database connection
    - connect to database
    
    [Delivers #163585039]

--- src/routes/partyRoutes.js
+++ src/routes/partyRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import PartyController from '../controllers/partyController';
 import Validate from '../middlewares/validator';
-import AuthUser from '../middlewares/authUser';
 
 const router = express.Router();
 
 router.get('/parties', PartyController.getAllParties);
-router.post('/parties', AuthUser.verifyAdmin, Validate.validatePartyName, Validate.validateHqAddress, Validate.validateLogoUrl, PartyController.createParty);
+router.post('/parties',  Validate.validatePartyName, Validate.validateHqAddress, Validate.validateLogoUrl, PartyController.createParty);
 router.get('/parties/:id', Validate.findPartyById, PartyController.getPartyById);
-router.patch('/parties/:id/name', AuthUser.verifyAdmin, Validate.findPartyById, Validate.validatePartyName, PartyController.updatePartyName);
-router.delete('/parties/:id', AuthUser.verifyAdmin, Validate.findPartyById, PartyController.deletePartyById);
+router.patch('/parties/:id/name', Validate.findPartyById, Validate.validatePartyName, PartyController.updatePartyName);
+router.delete('/parties/:id', Validate.findPartyById, PartyController.deletePartyById);
 
 export default router;
