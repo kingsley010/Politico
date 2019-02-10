655e4349671ca6a5824ff46a83ec20032778baef
Kingsley <perfectman090@gmail.com>
2019-02-10 10:46:09 +0100

    herokuConnect

--- src/routes/officeRoutes.js
+++ src/routes/officeRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import OfficeController from '../controllers/officeController';
 import Validate from '../middlewares/validator';
+import AuthUser from '../middlewares/authUser';
 
 const router = express.Router();
 
-router.post('/offices', Validate.officeType, Validate.officeName, OfficeController.createOffice);
+router.post('/offices', AuthUser.verifyAdmin, Validate.officeType, Validate.officeName, OfficeController.createOffice);
 router.get('/offices', OfficeController.getAllOffice);
 router.get('/offices/:id', Validate.findOfficeById, OfficeController.getOfficeById);
 
